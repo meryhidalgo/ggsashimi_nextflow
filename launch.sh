b3f5a8c2b5f002476214aea8516d214e6c035ec9
María Carazo <mariacarazohidalgo@gmail.com>
2026-09-07 16:01:14 +0200

    cluster options

--- launch.sh
+++ launch.sh
@@ -3,10 +3,15 @@
 #SBATCH --time=20:00:00
 #SBATCH --mem=8G
 
-module load Singularity Nextflow Go
+module load Nextflow Apptainer
 
 # limit the resources of the JVM
 export NXF_OPTS="-Xms500M -Xmx8G"
 
 # launch the main process
-nextflow run main.nf -resume -profile cluster -params-file input_params.yaml
+cd ggsashimi_nextflow {dir}
+nextflow run main.nf -resume -profile local_apptainer -params-file input_params.yaml
+
+#nextflow run main.nf -resume -profile cluster -params-file input_params.yaml # for ATLAS CLUSTER
+#nextflow run main.nf -resume -profile cluster_apptainer -params-file input_params.yaml # for HYPERION
+#nextflow run main.nf -resume -profile local_apptainer -params-file input_params.yaml # for HYPERION WHEN LAUNCHING IT INSIDE A NODE
